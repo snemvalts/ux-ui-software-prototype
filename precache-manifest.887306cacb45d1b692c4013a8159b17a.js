541b154f6bde3375c8d05920fb1fa57134b112bc
Sander Nemvalts <snemvalts@gmail.com>
2020-05-10 16:17:34 +0300

    new version

---
+++ precache-manifest.887306cacb45d1b692c4013a8159b17a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1798b81055549c88579bfff71e34b520",
+    "url": "/ux-ui-software-prototype/index.html"
+  },
+  {
+    "revision": "721536ea069deb1c8f6e",
+    "url": "/ux-ui-software-prototype/static/css/main.885ad857.chunk.css"
+  },
+  {
+    "revision": "fbd889e3f48477845b84",
+    "url": "/ux-ui-software-prototype/static/js/2.e3a7faf4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ux-ui-software-prototype/static/js/2.e3a7faf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "721536ea069deb1c8f6e",
+    "url": "/ux-ui-software-prototype/static/js/main.203662fc.chunk.js"
+  },
+  {
+    "revision": "4e85af80f62a19d300d4",
+    "url": "/ux-ui-software-prototype/static/js/runtime-main.b03e983b.js"
+  }
+]);
